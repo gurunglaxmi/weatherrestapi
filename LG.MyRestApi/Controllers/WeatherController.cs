c7c28db9b14791f7b7fe472f1eb6c6408a63cbed
LaxmiGurung <gurunglaxmi855@gmail.com>
2021-02-08 22:01:20 -0800

    initial weather api app

---
+++ LG.MyRestApi/Controllers/WeatherController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LG.MyRestApi.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/weather")]
+    public class WeatherController : Controller
+    {
+        [HttpGet]
+        [Route("timestamp")]
+        public IActionResult Time([FromQuery]string name)
+        {
+            var now = DateTime.Now;
+            return Ok(new { CurrentTimeUTC = now.ToUniversalTime(), CurrentTimeServer = now, UserProvidedName = name });
+        }
+    }
+}
---
+++ LG.MyRestApi/Helpers/SnakeCaseNamingPolicy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json;
+
+namespace LG.MyRestApi.Helpers
+{
+    public class SnakeCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            int maxLength = (int)Math.Ceiling(1.5 * name.Length) - 1;
+            char[] chars = new char[maxLength];
+            var charIndex = 0;
+            var capChar = true;
+
+            foreach (var c in name)
+            {
+                if (char.IsUpper(c))
+                {
+                    if (!capChar)
+                    {
+                        chars[charIndex++] = '_';
+                        capChar = true;
+                    }
+
+                }
+                else
+                {
+                    capChar = false;
+                }
+                chars[charIndex++] = char.ToLower(c);
+            }
+            return new string(chars, 0, charIndex);
+        }
+    }
+}
